88edf397f7811acb82211f286e37cb5d9ef1136f
1Shyy <wwe.sobolev@yandex.ru>
2023-10-29 22:15:55 +0300

    final 2.0

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -68,31 +68,107 @@ function deleteItem(clas) {
 }
 
 $('.tab-content ul').scroll(function() {
-        const verticalScrollPx = $('.tab-content ul').scrollTop();
+        const verticalScrollPx1 = $('#px1').scrollTop();
+        const verticalScrollPx2 = $('#px2').scrollTop();
+        const verticalScrollPx3 = $('#px3').scrollTop();
+        const verticalScrollPx4 = $('#px4').scrollTop();
         var count = $(':checkbox:checked').length;
-        console.log(verticalScrollPx)
+        if (verticalScrollPx1 > 0) {
                 if (count === 0) {
-                    if (verticalScrollPx < 15 && verticalScrollPx > 0) {
-                        $('.before').css('display', 'none')
-                        
+                    if (verticalScrollPx1 < 15 && verticalScrollPx1 > 0) {
+                        $('.before').css('display', 'none')                 
                     }
-                    else if (verticalScrollPx > 14 && verticalScrollPx < 30) {
+                    else if (verticalScrollPx1 > 14 && verticalScrollPx1 < 30) {
                         $('.before').css('display', 'block')
                         $('.after').css('display', 'block')
                     }
-                    if (verticalScrollPx > 31) {
+                    if (verticalScrollPx1 > 31) {
                         $('.after').css('display', 'none')
                     }
                 } else {
-                    if (verticalScrollPx < 15) {
+                    if (verticalScrollPx1 < 15) {
                         $('.before').css('display', 'none')
                     }
-                    else if (verticalScrollPx > 14 && verticalScrollPx < 86) {
+                    else if (verticalScrollPx1 > 14 && verticalScrollPx1 < 86) {
                         $('.before').css('display', 'block')
                         $('.after').css('display', 'block')
                     }
-                    else if (verticalScrollPx > 85) {
+                    else if (verticalScrollPx1 > 85) {
                         $('.after').css('display', 'none')
                     }
                 }
+        } else if (verticalScrollPx2 > 0) {
+            if (count === 0) {
+                if (verticalScrollPx2 < 15 && verticalScrollPx2 > 0) {
+                    $('.before').css('display', 'none')                 
+                }
+                else if (verticalScrollPx2 > 14 && verticalScrollPx2 < 30) {
+                    $('.before').css('display', 'block')
+                    $('.after').css('display', 'block')
+                }
+                if (verticalScrollPx2 > 31) {
+                    $('.after').css('display', 'none')
+                }
+            } else {
+                if (verticalScrollPx2 < 15) {
+                    $('.before').css('display', 'none')
+                }
+                else if (verticalScrollPx2 > 14 && verticalScrollPx2 < 86) {
+                    $('.before').css('display', 'block')
+                    $('.after').css('display', 'block')
+                }
+                else if (verticalScrollPx2 > 85) {
+                    $('.after').css('display', 'none')
+                }
+            }
+        } else if (verticalScrollPx3 > 0) {
+            if (count === 0) {
+                if (verticalScrollPx3 < 15 && verticalScrollPx3 > 0) {
+                    $('.before').css('display', 'none')                 
+                }
+                else if (verticalScrollPx3 > 14 && verticalScrollPx3 < 30) {
+                    $('.before').css('display', 'block')
+                    $('.after').css('display', 'block')
+                }
+                if (verticalScrollPx3 > 31) {
+                    $('.after').css('display', 'none')
+                }
+            } else {
+                if (verticalScrollPx3 < 15) {
+                    $('.before').css('display', 'none')
+                }
+                else if (verticalScrollPx3 > 14 && verticalScrollPx3 < 86) {
+                    $('.before').css('display', 'block')
+                    $('.after').css('display', 'block')
+                }
+                else if (verticalScrollPx3 > 85) {
+                    $('.after').css('display', 'none')
+                }
+            }
+        } else if (verticalScrollPx4 > 0) {
+            if (count === 0) {
+                if (verticalScrollPx4 < 15 && verticalScrollPx4 > 0) {
+                    $('.before').css('display', 'none')                 
+                }
+                else if (verticalScrollPx4 > 14 && verticalScrollPx4 < 30) {
+                    $('.before').css('display', 'block')
+                    $('.after').css('display', 'block')
+                }
+                if (verticalScrollPx4 > 31) {
+                    $('.after').css('display', 'none')
+                }
+            } else {
+                if (verticalScrollPx4 < 15) {
+                    $('.before').css('display', 'none')
+                }
+                else if (verticalScrollPx4 > 14 && verticalScrollPx4 < 86) {
+                    $('.before').css('display', 'block')
+                    $('.after').css('display', 'block')
+                }
+                else if (verticalScrollPx4 > 85) {
+                    $('.after').css('display', 'none')
+                }
+            }
+        }
+
 });
